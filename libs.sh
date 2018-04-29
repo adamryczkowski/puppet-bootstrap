67ed381595f30c15fb7b4cef38f264d5d94407d2
adam <aryczkowski@imgw.pl>
2018-04-29 19:22:46 +0200

    bugfix

--- libs.sh
+++ libs.sh
@@ -66,7 +66,7 @@ function install_apt_package_file {
 	local source="$3"
 	if ! dpkg -s "$package">/dev/null  2> /dev/null; then
 		local tmp=$(dirname ${filename})
-		if [ "${tmp}" == "" ]; then
+		if [ "${tmp}" == "." ]; then
 			local cfilename=$(get_cached_file "${filename}" "$source")
 			if [ ! -f "${cfilename}" ]; then
 				errcho "Cannot find ${filename}"
