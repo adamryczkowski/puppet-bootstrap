81fd0a5a32cb57a1812df8525ee2c10a4154e9e8
adam <aryczkowski@imgw.pl>
2018-06-13 06:29:21 +0200

    lxc

--- configure-lxd.sh
+++ configure-lxd.sh
@@ -143,7 +143,7 @@ if ! grep -q "root:$UID:1" /etc/subgid; then
     echo "root:$UID:1" | sudo tee -a /etc/subgid
 fi
 
-the_ppa=ubuntu-lxc/lxc-stable
+the_ppa=ubuntu-lxc/lxc-git-stable-2.0
 add_ppa $the_ppa
 do_upgrade
 
