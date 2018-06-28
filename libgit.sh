4c236a5dcef4aafca246be517c3ff60b6e91c09e
adam <aryczkowski@imgw.pl>
2018-06-28 15:18:27 +0200

    fix

--- libgit.sh
+++ libgit.sh
@@ -18,3 +18,43 @@ function make_sure_git_exists {
 		logexec sudo --user "$user" -- git init "${repo_path}"
 	fi
 }
+
+
+function get_git_repo {
+	local repo=$1
+	local dir=$2
+	local name=$3
+	if [ -z "${name}" ]; then
+		local pattern='^.*/([^/]+)(\.git)$'
+		if [[ "$repo" =~ $pattern ]]; then
+			name=${BASH_REMATCH[1]}
+		else
+			local pattern='^.*/([^/]+)$'
+			if [[ "$repo" =~ $pattern ]]; then
+				name=${BASH_REMATCH[1]}
+			else
+				errcho "${repo} has wrong format"
+			fi
+		fi
+	fi
+	local dest=${dir}/${name}
+	install_apt_package git
+	if [ ! -d "$dir" ]; then
+		errcho "Cannot find ${dir} directory to clone git repo ${repo}"
+		return 1
+	fi
+	if [ -w "$dir" ]; then
+		local prefix=""
+	else
+		local prefix="sudo "
+	fi
+	
+	if [ -d ${dest} ]; then
+		# update repo
+		logexec $prefix git pull
+	else
+		# clone repo
+		logexec $prefix git clone --depth 1 ${repo} ${dest}
+	fi
+}
+
--- libs.sh
+++ libs.sh
@@ -174,44 +174,6 @@ function get_ui_context {
 }
 
 
-function get_git_repo {
-	local repo=$1
-	local dir=$2
-	local name=$3
-	if [ -z "${name}" ]; then
-		local pattern='^.*/([^/]+)(\.git)$'
-		if [[ "$repo" =~ $pattern ]]; then
-			name=${BASH_REMATCH[1]}
-		else
-			local pattern='^.*/([^/]+)$'
-			if [[ "$repo" =~ $pattern ]]; then
-				name=${BASH_REMATCH[1]}
-			else
-				errcho "${repo} has wrong format"
-			fi
-		fi
-	fi
-	local dest=${dir}/${name}
-	install_apt_package git
-	if [ ! -d "$dir" ]; then
-		errcho "Cannot find ${dir} directory to clone git repo ${repo}"
-		return 1
-	fi
-	if [ -w "$dir" ]; then
-		local prefix=""
-	else
-		local prefix="sudo "
-	fi
-	
-	if [ -d ${dest} ]; then
-		# update repo
-		logexec $prefix git pull
-	else
-		# clone repo
-		logexec $prefix git clone --depth 1 ${repo} ${dest}
-	fi
-}
-
 function make_desktop_non_script {
 	local execpath="$1"
 	local title="$2"
@@ -240,7 +202,7 @@ Categories=Application;"
 
 function make_service_user {
 	local username=$1
-	if ! id -u name 2>/dev/null; then
+	if ! id -u $username 2>/dev/null; then
 		logexec sudo useradd -r --shell /bin/false $username 
 	fi
 }
