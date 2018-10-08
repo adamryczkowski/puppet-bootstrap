f11fd7e50f14751f12b54235a40d8eec0db73d12
adam <adam@statystyka.net>
2018-10-08 20:08:22 +0200

    update dst

---
+++ files/dst_worldgenoverride1.lua
@@ -0,0 +1,19 @@
+return {
+ 
+       override_enabled = true,
+       misc = {
+               start_location = "plus",
+               season_start = "autumn",
+               world_size = "huge"
+       },
+       monsters = { -- "never", "rare", "default", "often", "always"
+               bearger = "default",
+               chess = "default",
+--               deerclops = "often"
+       },
+       resources = { -- "never", "rare", "default", "often", "always"
+               meteorshowers = "default",
+               meteorspawner = "rare"
+       },
+}
+
---
+++ files/dst_worldgenoverride2.lua
@@ -0,0 +1,20 @@
+return {
+ 
+       override_enabled = true,
+       preset = "DST_CAVE",
+       misc = {
+--               start_location = "plus",
+               season_start = "autumn",
+               world_size = "huge"
+       },
+       monsters = { -- "never", "rare", "default", "often", "always"
+               bearger = "default",
+--               chess = "often",
+               deerclops = "often"
+       },
+       resources = { -- "never", "rare", "default", "often", "always"
+               meteorshowers = "never",
+               meteorspawner = "never"
+       },
+}
+
