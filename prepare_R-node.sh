cb566610408ee6842c648daad981a64181a1a1c2
Adam Ryczkowski <adam@statystyka.net>
2020-07-06 20:10:20 +0200

    R 4.0

--- prepare_R-node.sh
+++ prepare_R-node.sh
@@ -119,7 +119,7 @@ fi
 if ! grep -q "^deb .*https://cran.rstudio.com" /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
 	release=$(get_ubuntu_codename)
 	$loglog
-	echo "deb https://cloud.r-project.org/bin/linux/ubuntu ${release}-cran35/" | sudo tee /etc/apt/sources.list.d/r.list
+	echo "deb https://cloud.r-project.org/bin/linux/ubuntu ${release}-cran40/" | sudo tee /etc/apt/sources.list.d/r.list
 	logexec gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 	$loglog
 	sudo gpg -a --export E084DAB9 | sudo apt-key add -
